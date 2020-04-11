d79dc6e2f9dc45a61955efe0369e0c40514388dd
Tiemen <Tiemen2@live.nl>
2020-04-11 17:40:58 +0200

    add gateway intents

--- disco/client.py
+++ disco/client.py
@@ -28,6 +28,8 @@ class ClientConfig(Config):
         The total count of shards running.
     guild_subscriptions : bool
         Whether to enable subscription events (e.g. presence and typing).
+    intents : int
+        Defines which events the bot will receive from the API.
     max_reconnects : int
         The maximum number of connection retries to make before giving up (0 = never give up).
     log_level: str
@@ -45,7 +47,8 @@ class ClientConfig(Config):
     token = ''
     shard_id = 0
     shard_count = 1
-    guild_subscriptions = False
+    guild_subscriptions = True
+    intents = 513
     max_reconnects = 5
     log_level = 'info'
 
